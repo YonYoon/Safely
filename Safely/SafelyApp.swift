31c962be3b02b1c5bcb23472c940f161cd41915d
YonYoon <zhzhzh1806@gmail.com>
2024-07-04 13:03:17 +0500

    Initial Commit

---
+++ Safely/SafelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafelyApp.swift
+//  Safely
+//
+//  Created by Zhansen Zhalel on 04.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SafelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
