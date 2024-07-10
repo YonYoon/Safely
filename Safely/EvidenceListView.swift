bef9eaca3cfef1f3c39557bcea544a476ff70555
YonYoon <zhzhzh1806@gmail.com>
2024-07-10 22:37:25 +0500

    Create two new views and tutorial model

---
+++ Safely/EvidenceListView.swift
@@ -0,0 +1,18 @@
+//
+//  EvidenceListView.swift
+//  Safely
+//
+//  Created by Zhansen Zhalel on 10.07.2024.
+//
+
+import SwiftUI
+
+struct EvidenceListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EvidenceListView()
+}
---
+++ Safely/Tutorial.swift
@@ -0,0 +1,8 @@
+//
+//  Tutorial.swift
+//  Safely
+//
+//  Created by Zhansen Zhalel on 10.07.2024.
+//
+
+import Foundation
