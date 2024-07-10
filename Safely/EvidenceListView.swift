3c80bb6e2c6942fc22e08d80405e9d9cf4794b16
YonYoon <zhzhzh1806@gmail.com>
2024-07-10 22:41:14 +0500

    Make UI for EvidenceListView

--- Safely/EvidenceListView.swift
+++ Safely/EvidenceListView.swift
@@ -9,7 +9,33 @@ import SwiftUI
 
 struct EvidenceListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                HStack {
+                    Text("Возможность записывать аудио, видео и текстовые сообщения, которые автоматически сохраняются в облаке и могут быть использованы как доказательства в случае необходимости.")
+                    
+                    Spacer()
+                }
+                .padding(.bottom, 100)
+                
+                Button {
+                    // Load evidence
+                } label: {
+                    VStack {
+                        Image(systemName: "folder.fill.badge.plus")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100)
+                        
+                        Text("Загрузить аудио/видео/текст")
+                    }
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("Доказательства")
+        }
     }
 }
 
