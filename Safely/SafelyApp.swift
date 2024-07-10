711ae7fca8486af5a6f3a3bcccb9dfa9e1a5b890
YonYoon <zhzhzh1806@gmail.com>
2024-07-10 22:38:49 +0500

    Add new tab items

--- Safely/SafelyApp.swift
+++ Safely/SafelyApp.swift
@@ -16,10 +16,16 @@ struct SafelyApp: App {
         WindowGroup {
             TabView {
                 SosView()
-                    .tabItem { Label("Экстренный вызов", systemImage: "sos.circle") }
+                    .tabItem { Label("Помощь", systemImage: "sos.circle") }
                 
                 ProfileView(profile: profile)
                     .tabItem { Label("Профиль", systemImage: "person.circle.fill") }
+                
+                TutorialsView()
+                    .tabItem { Label("Обучение", systemImage: "graduationcap.circle") }
+                
+                EvidenceListView()
+                    .tabItem { Label("Хранение", systemImage: "lock.circle") }
             }
         }
     }
